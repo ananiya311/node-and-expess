b6e9fb27272544605ad8de0e0a543d9e49faa88b
ananiya muluken <ananiyakok@gmail.com>
2022-07-17 21:44:06 +0300

    test

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
 var app = express();
+require('dotenv').config()
+
+app.use((req, res, next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/views/index.html")
+})
+app.get('/json', (req, res)=>{
+  var respons 
+  if(process.env.MESSAGE_STYLE == "allCaps"){
+    respons = "Hellow Json".toUpperCase()
+  }else{
+    respons = "Hellow Json"
+  }
+  res.json({"message": respons})
+})
+
+app.get("/now", (req, res, next)=>{
+  req.time = new Date().toString()
+  next()
+},function(req,res){
+  res.json({time: req.time})
+})
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
